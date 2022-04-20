9df48641c0cfe7fa3a26f940270b4ce9e01fb148
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2022-04-19 20:49:52 -0600

    [BIA-1068] Create new view DimK12Schools for the the CEDS collection
    - Update TableInformation populate script.
    - Add K12SchoolDim view.
    - Rename K12DemographicDim-Create to add a new sequence number.

--- CEDS/PostgreSQL/0004-Table-EdfiTableInformation-Insert.sql
+++ CEDS/PostgreSQL/0004-Table-EdfiTableInformation-Insert.sql
@@ -3,39 +3,46 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-WITH source AS (VALUES
-	( -1, '-1', 'MISSING', 4),
-	(471, 'Preschool/Prekindergarten', 'PK', 4),
-	(467, 'Kindergarten', 'KG', 4),
-	(463, 'First grade', '3', 4),
-	(472, 'Second grade', '4', 4),
-	(476, 'Third grade', '5', 4),
-	(464, 'Fourth grade', '6', 4),
-	(462, 'Fifth grade', '7', 4),
-	(474, 'Sixth grade', '8', 4),
-	(473, 'Seventh grade', '9', 4),
-	(460, 'Eighth grade', '10', 4),
-	(468, 'Ninth grade', '11', 4),
-	(475, 'Tenth grade', '10', 4),
-	(461, 'Eleventh grade', '11', 4),
-	(477, 'Twelfth grade', '12', 4),
-	(470, 'Postsecondary', 'HS', 4),
-	(478, 'Ungraded', 'UG', 4),
-	(458, 'Adult Education', 'AE', 4)
-)
-INSERT INTO 
-    xref.EdfiTableInformation
-(
-	EdFiDescriptorId,
-	EdFiCodeValue,
-	EdFactsCode,
-	EdFiTableId
-)
-SELECT
-    source.column1,
-	source.column2,
-	source.column3,
-	source.column4
+WITH SOURCE AS (SELECT Descriptor.DescriptorId
+	, Descriptor.CodeValue
+	, MapReference.EdFactsCode
+	, MapReference.EdFiTableId
 FROM
-    source
+	(VALUES
+		('uri://ed-fi.org/GradeLevelDescriptor','Preschool/Prekindergarten', 'PK', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Kindergarten', 'KG', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','First grade', '3', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Second grade', '4', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Third grade', '5', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Fourth grade', '6', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Fifth grade', '7', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Sixth grade', '8', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Seventh grade', '9', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Eighth grade', '10', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Ninth grade', '11', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Tenth grade', '10', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Eleventh grade', '11', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Twelfth grade', '12', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Postsecondary', 'HS', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Ungraded', 'UG', 4),
+		('uri://ed-fi.org/GradeLevelDescriptor','Adult Education', 'AE', 4)
+	) MapReference (Namespace, CodeValue, EdFactsCode, EdFiTableId)
+INNER JOIN 
+	edfi.Descriptor 
+		ON MapReference.CodeValue = Descriptor.CodeValue
+			AND Descriptor.Namespace='uri://ed-fi.org/GradeLevelDescriptor'
+)
+  INSERT INTO xref.EdfiTableInformation
+	  (
+		EdFiDescriptorId
+		, EdFiCodeValue
+		, EdFactsCode
+		, EdFiTableId
+	  )
+    SELECT
+        Source.DescriptorId
+		  , Source.CodeValue
+		  , Source.EdFactsCode
+		  , Source.EdFiTableId
+    FROM Source
 ON CONFLICT DO NOTHING;
