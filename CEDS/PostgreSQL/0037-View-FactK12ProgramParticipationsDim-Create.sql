3c8e9eecdd12747662d1aedf00d6f7abd98d7560
jegasape <jegasape@gmail.com>
2022-07-19 17:25:16 -0400

    [BIA-1130] Addressing comments and doing code clean-up

--- CEDS/PostgreSQL/0037-View-FactK12ProgramParticipationsDim-Create.sql
+++ CEDS/PostgreSQL/0037-View-FactK12ProgramParticipationsDim-Create.sql
@@ -17,16 +17,16 @@ AS
             GROUP BY EducationOrganizationId, ProgramTypeDescriptorId, ProgramEducationOrganizationId
 	    ) AS FactK12ProgramParticipationId,
            SchoolYearKey
-           ,'' AS DateId
-           ,'' AS DataCollectionId
-           ,SeaIdentifierSea AS SeaId
-           ,LeaIdentifierSea AS IeuId
-           ,LeaIdentifierNces AS LeaId
-           ,K12SchoolKey AS K12SchoolId
-           ,K12ProgramTypeKey AS DimK12ProgramTypeId
-           ,StudentSchoolKey AS DimK12StudentlId
-           ,K12DemographicKey AS DimK12DemographicId
-           ,IdeaStatusKey AS DimIdeaStatusId
+           ,'' AS DateKey
+           ,'' AS DataCollectionKey
+           ,IeuOrganizationIdentifierSea AS SeaKey
+           ,LeaIdentifierSea AS IeuKey
+           ,LeaIdentifierNces AS LeaKey
+           ,K12SchoolKey AS K12SchoolKey
+           ,K12ProgramTypeKey AS DimK12ProgramTypeKey
+           ,StudentSchoolKey AS DimK12StudentlKey
+           ,K12DemographicKey AS DimK12DemographicKey
+           ,IdeaStatusKey AS DimIdeaStatusKey
            ,GeneralStudentProgramAssociation.BeginDate AS BeginDate
            ,GeneralStudentProgramAssociation.EndDate AS EndDate
         FROM
@@ -34,12 +34,12 @@ AS
         INNER JOIN
             analytics.ceds_SchoolYearsDim
         ON
-            StudentSpecialEducationProgramAssociation.BeginDate
-            BETWEEN TO_DATE(ceds_SchoolYearsDim.SessionBeginDate, 'MM-DD-YYYY') AND TO_DATE(ceds_SchoolYearsDim.SessionEndDate, 'MM-DD-YYYY')
+            StudentSpecialEducationProgramAssociation.BeginDate 
+               BETWEEN TO_DATE(ceds_SchoolYearsDim.SessionBeginDate, 'MM-DD-YYYY') AND TO_DATE(ceds_SchoolYearsDim.SessionEndDate, 'MM-DD-YYYY')
         INNER JOIN
             analytics.ceds_K12SchoolDim
         ON
-            ceds_K12SchoolDim.K12SchoolKey = StudentSpecialEducationProgramAssociation.EducationOrganizationId
+            ceds_K12SchoolDim.SchoolIdentifierSea = StudentSpecialEducationProgramAssociation.EducationOrganizationId
         INNER JOIN
             edfi.ProgramTypeDescriptor
         ON
@@ -51,7 +51,7 @@ AS
         INNER JOIN
             analytics.ceds_K12ProgramTypeDim
         ON
-            Descriptor.CodeValue = ceds_K12ProgramTypeDim.ProgramTypeCode
+           ceds_K12ProgramTypeDim.ProgramTypeCode = Descriptor.CodeValue
         INNER JOIN
             edfi.Student
         ON
@@ -63,88 +63,144 @@ AS
         LEFT JOIN
             edfi.StudentEducationOrganizationAssociation
         ON
-            StudentEducationOrganizationAssociation.StudentUSI = StudentSpecialEducationProgramAssociation.StudentUSI AND Descriptor.Namespace = StudentEducationOrganizationAssociation.SexDescriptorId
-        INNER JOIN
-            edfi.StudentEducationOrganizationAssociationStudentCharacteristic
-        ON
-            StudentEducationOrganizationAssociationStudentCharacteristic.EducationOrganizationId = StudentSpecialEducationProgramAssociation.EducationOrganizationId
-        INNER JOIN
+            StudentEducationOrganizationAssociation.StudentUSI = StudentSpecialEducationProgramAssociation.StudentUSI 
+            AND Descriptor.DescriptorId = StudentEducationOrganizationAssociation.SexDescriptorId
+        LEFT JOIN
+			edfi.StudentEducationOrganizationAssociationStudentCharacteristic EconomicDisadvantageCharacteristic
+				ON StudentEducationOrganizationAssociation.StudentUSI = EconomicDisadvantageCharacteristic.StudentUSI
+		LEFT JOIN
+			analytics_config.ceds_TableInformation EconomicDisadvantage
+				ON EconomicDisadvantageCharacteristic.StudentCharacteristicDescriptorId = EconomicDisadvantage.DescriptorId
+		LEFT JOIN
+			analytics.ceds_K12DemographicDim
+				ON EconomicDisadvantage.CodeValue = ceds_K12DemographicDim.EconomicDisadvantageStatusCode
+		LEFT JOIN
+			edfi.StudentEducationOrganizationAssociationStudentCharacteristic HomelessnessCharacteristic
+				ON StudentEducationOrganizationAssociation.StudentUSI = HomelessnessCharacteristic.StudentUSI
+		LEFT JOIN
+			analytics_config.ceds_TableInformation Homelessness
+				ON HomelessnessCharacteristic.StudentCharacteristicDescriptorId = Homelessness.DescriptorId
+					AND ceds_K12DemographicDim.HomelessnessStatusCode = Homelessness.CodeValue
+		LEFT JOIN
+			edfi.StudentEducationOrganizationAssociationStudentCharacteristic EnglishLearnerCharacteristic
+				ON StudentEducationOrganizationAssociation.StudentUSI = EnglishLearnerCharacteristic.StudentUSI
+		LEFT JOIN
+			analytics_config.ceds_TableInformation EnglishLearner
+				ON EnglishLearnerCharacteristic.StudentCharacteristicDescriptorId = EnglishLearner.DescriptorId
+					AND ceds_K12DemographicDim.EnglishLearnerStatusCode = EnglishLearner.CodeValue
+		LEFT JOIN
+			edfi.StudentEducationOrganizationAssociationStudentCharacteristic MigrantCharacteristic
+				ON StudentEducationOrganizationAssociation.StudentUSI = MigrantCharacteristic.StudentUSI
+		LEFT JOIN
+			analytics_config.ceds_TableInformation Migrant
+				ON MigrantCharacteristic.StudentCharacteristicDescriptorId = Migrant.DescriptorId
+					AND ceds_K12DemographicDim.MigrantStatusCode = Migrant.CodeValue
+		LEFT JOIN
+			edfi.StudentEducationOrganizationAssociationStudentCharacteristic MilitaryConnectedCharacteristic
+				ON StudentEducationOrganizationAssociation.StudentUSI = MilitaryConnectedCharacteristic.StudentUSI
+		LEFT JOIN
+			analytics_config.ceds_TableInformation MilitaryConnected
+				ON MilitaryConnectedCharacteristic.StudentCharacteristicDescriptorId = MilitaryConnected.DescriptorId
+					AND ceds_K12DemographicDim.MilitaryConnectedStudentIndicatorCode = MilitaryConnected.CodeValue
+		LEFT JOIN
+			edfi.StudentEducationOrganizationAssociationStudentCharacteristic HomelessPrimaryNighttimeResidenceCharacteristic
+				ON StudentEducationOrganizationAssociation.StudentUSI = HomelessPrimaryNighttimeResidenceCharacteristic.StudentUSI
+		LEFT JOIN
+			analytics_config.ceds_TableInformation HomelessPrimaryNighttimeResidence
+				ON HomelessPrimaryNighttimeResidenceCharacteristic.StudentCharacteristicDescriptorId = HomelessPrimaryNighttimeResidence.DescriptorId
+					AND ceds_K12DemographicDim.HomelessPrimaryNighttimeResidenceCode = HomelessPrimaryNighttimeResidence.CodeValue
+		LEFT JOIN
+			edfi.StudentEducationOrganizationAssociationStudentCharacteristic HomelessUnaccompaniedYouthCharacteristic
+				ON StudentEducationOrganizationAssociation.StudentUSI = HomelessUnaccompaniedYouthCharacteristic.StudentUSI
+		LEFT JOIN
+			analytics_config.ceds_TableInformation HomelessUnaccompaniedYouth
+				ON HomelessUnaccompaniedYouthCharacteristic.StudentCharacteristicDescriptorId = HomelessUnaccompaniedYouth.DescriptorId
+					AND ceds_K12DemographicDim.HomelessUnaccompaniedYouthStatusCode = HomelessUnaccompaniedYouth.CodeValue
+		LEFT JOIN
+			analytics_config.ceds_TableInformation SexCode
+				ON StudentEducationOrganizationAssociation.SexDescriptorId = SexCode.DescriptorId
+					AND ceds_K12DemographicDim.SexCode = SexCode.CodeValue
+        LEFT JOIN
             edfi.StudentHomelessProgramAssociation
-        ON
-            StudentHomelessProgramAssociation.StudentUSI = StudentEducationOrganizationAssociation.StudentUSI
-        INNER JOIN
-            analytics.ceds_K12DemographicDim
-        ON
-            ceds_K12DemographicDim.K12DemographicKey = StudentHomelessProgramAssociation.ProgramName
+                ON StudentHomelessProgramAssociation.StudentUSI = StudentEducationOrganizationAssociation.StudentUSI
         INNER JOIN
             edfi.GeneralStudentProgramAssociation
-        ON
-            GeneralStudentProgramAssociation.BeginDate = StudentSpecialEducationProgramAssociation.BeginDate OR
-            GeneralStudentProgramAssociation.EducationOrganizationId = StudentSpecialEducationProgramAssociation.EducationOrganizationId OR
-            GeneralStudentProgramAssociation.ProgramEducationOrganizationId = StudentSpecialEducationProgramAssociation.ProgramEducationOrganizationId OR
-            GeneralStudentProgramAssociation.ProgramName = StudentSpecialEducationProgramAssociation.ProgramName OR
-            GeneralStudentProgramAssociation.ProgramTypeDescriptorId = StudentSpecialEducationProgramAssociation.ProgramTypeDescriptorId OR
-            GeneralStudentProgramAssociation.StudentUSI = StudentSpecialEducationProgramAssociation.StudentUSI
-        INNER JOIN
-            analytics.ceds_IdeaStatusDim
-        ON
-            ceds_IdeaStatusDim.DisabilityCode = Descriptor.CodeValue AND ceds_IdeaStatusDim.BasisOfExitCode = Descriptor.CodeValue
+                ON
+                    GeneralStudentProgramAssociation.BeginDate = StudentSpecialEducationProgramAssociation.BeginDate
+                    OR GeneralStudentProgramAssociation.EducationOrganizationId = StudentSpecialEducationProgramAssociation.EducationOrganizationId
+                    OR GeneralStudentProgramAssociation.ProgramEducationOrganizationId = StudentSpecialEducationProgramAssociation.ProgramEducationOrganizationId
+                    OR GeneralStudentProgramAssociation.ProgramName = StudentSpecialEducationProgramAssociation.ProgramName
+                    OR GeneralStudentProgramAssociation.ProgramTypeDescriptorId = StudentSpecialEducationProgramAssociation.ProgramTypeDescriptorId
+                    OR GeneralStudentProgramAssociation.StudentUSI = StudentSpecialEducationProgramAssociation.StudentUSI
+        INNER JOIN 
+            edfi.StudentSpecialEducationProgramAssociationDisability
+                ON
+                    StudentSpecialEducationProgramAssociationDisability.BeginDate = StudentSpecialEducationProgramAssociation.BeginDate
+                    OR StudentSpecialEducationProgramAssociationDisability.EducationOrganizationId = StudentSpecialEducationProgramAssociation.EducationOrganizationId
+                    OR StudentSpecialEducationProgramAssociationDisability.ProgramEducationOrganizationId = StudentSpecialEducationProgramAssociation.ProgramEducationOrganizationId
+                    OR StudentSpecialEducationProgramAssociationDisability.ProgramTypeDescriptorId = StudentSpecialEducationProgramAssociation.ProgramTypeDescriptorId
+                    OR StudentSpecialEducationProgramAssociationDisability.StudentUSI = StudentSpecialEducationProgramAssociation.StudentUSI
+        INNER JOIN 
+            analytics.ceds_IdeaStatusDim DimIdeaStatus
+                ON
+                    Descriptor.CodeValue = DimIdeaStatus.BasisOfExitCode 
+                    AND Descriptor.CodeValue = DimIdeaStatus.DisabilityCode
         )
     SELECT
         CONCAT(
             SchoolYearKey,
 			'-',
-			DateId,
+			DateKey,
 			'-',
-			DataCollectionId,
+			DataCollectionKey,
 			'-',
-			SeaId,
+			SeaKey,
 			'-',
-			IeuId,
+			IeuKey,
 			'-',
-			LeaId,
+			LeaKey,
 			'-',
-			K12SchoolId,
+			K12SchoolKey,
 			'-',
-			DimK12ProgramTypeId,
+			DimK12ProgramTypeKey,
 			'-',
-			DimK12StudentlId,
+			DimK12StudentlKey,
 			'-',
-			DimK12DemographicId,
+			DimK12DemographicKey,
 			'-',
-			DimIdeaStatusId,
+			DimIdeaStatusKey,
 			'-',
 			BeginDate,
 			'-',
 			EndDate
         ) AS FactK12ProgramParticipationKey
         ,SchoolYearKey
-        ,DateId
-        ,DataCollectionId
-        ,SeaId
-        ,IeuId
-        ,LeaId
-        ,K12SchoolId
-        ,DimK12ProgramTypeId
-        ,DimK12StudentlId
-        ,DimK12DemographicId
-        ,DimIdeaStatusId
+        ,DateKey
+        ,DataCollectionKey
+        ,SeaKey
+        ,IeuKey
+        ,LeaKey
+        ,K12SchoolKey
+        ,DimK12ProgramTypeKey
+        ,DimK12StudentlKey
+        ,DimK12DemographicKey
+        ,DimIdeaStatusKey
         ,BeginDate
         ,EndDate
-        ,COUNT(DimK12DemographicId) AS StudentCount
+        ,COUNT(DimK12DemographicKey) AS StudentCount
     FROM
         FactK12ProgramParticipation
     GROUP BY
-     SchoolYearKey
-    ,DateId
-    ,DataCollectionId
-    ,SeaId
-    ,IeuId
-    ,LeaId
-    ,K12SchoolId
-    ,DimK12ProgramTypeId
-    ,DimK12StudentlId
-    ,DimK12DemographicId
-    ,DimIdeaStatusId
-    ,BeginDate
-    ,EndDate
+        SchoolYearKey
+        ,DateKey
+        ,DataCollectionKey
+        ,SeaKey
+        ,IeuKey
+        ,LeaKey
+        ,K12SchoolKey
+        ,DimK12ProgramTypeKey
+        ,DimK12StudentlKey
+        ,DimK12DemographicKey
+        ,DimIdeaStatusKey
+        ,BeginDate
+        ,EndDate
