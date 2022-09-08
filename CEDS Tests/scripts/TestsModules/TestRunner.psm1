482b4c2966d6edc74a9c590017314bec65a33113
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-09-08 16:39:54 -0600

    [BIA-1196] Merge from develop to main (#42)
    * [BIA-1058] Create the xref tables necessary to relate the ODS to CEDS (#8)
    
    * [BIA-1074] Creates the xref tables necessary for Postgres. (#9)
    
    * Creates SeasDim on ceds collection.
    
    * StateAnsiCode instead of StateAnciCode
    
    * [BIA-1061] - Creates K12StudentsDim on CEDS collection.  (#12)
    
    * [BIA-1070] Create new view DimIdeaStatus for the the CEDS collection (#14)
    
    * [BIA-1066] - Creates LeusDim on ceds collection (#13)
    
    * Creates LeusDim on ceds collection.
    
    * Adds OutOfStateIndicator field.
    
    * Adds LeusDimKey field.
    
    * Script clean up.
    
    * [BIA-1073] Create new view DimK12Demographic (#15)
    
    * [BIA-1069] - Creates GradeLevelsDim on CEDS colletion. (#16)
    
    * [BIA-1068] Create new view DimK12Schools for the CEDS collection (#17)
    
    * [BIA-1067]  Create new view DimLeas for the the CEDS collection (#18)
    
    * [BIA-1067]  Create new view DimLeas for the the CEDS collection
    
    * Update script format
    
    * Update 0014-View-LeaDim-Create.sql
    
    * [BIA-1075] CEDS collection hardening (#20)
    
    * [BIA-1072] Create new view DimK12EnrollmentStatuses for the the CEDS collection (#19)
    
    * [BIA-1065] Create new view DimSchoolYears for the the CEDS collection (#21)
    
    * [BIA-1085] - Basic infrastructure to add unit tests for the Ceds Collection. (#22)
    
    * Creates infrastructure to implement tests for Ceds collection.
    
    * [BIA-1043] Adds tests for SeaDim view. (#23)
    
    * Adds tests for SeaDim view.
    
    * Encoding default.
    
    * [BIA-1046] Adds tests for K12SchoolDim view. (#27)
    
    * [BIA-1049] Adds tests for IdeaStatusDim view. (#28)
    
    * Adds tests for IdeaStatusDim view.
    
    * [BIA-1050] Adds tests for K12EnrollmentStatusDim view. (#29)
    
    * [BIA-1051] Adds tests for K12DemographicDim view. (#30)
    
    * Adds tests for LeaDim view. (#25)
    
    * Adds tests for IeuDim view. (#26)
    
    * [BIA-1127] Create DimK12ProgramTypes view for CEDS SPED membership (#32)
    
    Add view creation script.
    Add tests.
    
    * [BIA-1128]: Create RaceDim view for CEDS SPED membership (#33)
    
    * [BIA-1039] Adds tests for SchoolYearDim view. (#31)
    
    * Adds tests for SchoolYearsDim view.
    
    * Renames SchoolYearsDim view to SchoolYearDim.
    
    * [BIA-1052] Creates FactK12StudentEnrollments view. (#34)
    
    * Creates FactK12StudentEnrollments view.
    
    * Adds more fields to the view.
    
    * [BIA-1124] Update Ed-Fi descriptor to CEDS Code for CEDS AMT (#35)
    
    * Add MSSQL scripts
    
    * Add Postgresql scripts
    
    * [BIA-1158] Error querying ceds_K12SchoolDim
    
    Update column SchoolOperationalStatusEdFactsCode to be varchar.
    Update test cases.
    
    * [BIA-1130] Create FactK12ProgramParticipations view for CEDS SPED membership (#37)
    
    * [BIA-1129] Create BridgeK12StudentEnrollmentRaces view for CEDS SPED membership (#38)
    
    * Update 0018-View-FactK12StudentEnrollments-Create.sql
    
    * Rename view FactK12StudentEnrollment
    
    * Update testcases
    
    * [BIA-1083] Adds some sample data. (#39)
    
    * Adds some sample data.
    
    * Renaming Sample Data folder
    
    * [BIA-1126] Create DimDataCollection view for CEDS SPED membership (#40)
    
    MSSQL and Postgres
    - Add script to create DataCollectionDim table.
    - Add script to populate DataCollectionDim
    
    * Changes on the ceds_K12DemographicDim View on the Ceds collection. (#41)
    
    Co-authored-by: Jeffrey Sanchez <44595343+jegasape@users.noreply.github.com>
    Co-authored-by: José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
    Co-authored-by: José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>

---
+++ CEDS Tests/scripts/TestsModules/TestRunner.psm1
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+#Requires -RunAsAdministrator
+
+function Submit-TestMSSQL {
+	Param (
+        [Parameter(Mandatory = $true)]
+        [string] $connectionString,
+        [Parameter(Mandatory = $true)]
+        [string] $name,
+		[Parameter(Mandatory = $true)]
+        [string] $query,
+		[Parameter(Mandatory = $true)]
+        [string] $executionResultsPath
+    )
+    
+	Invoke-Sqlcmd -Query $query -ConnectionString $connectionString | Export-Csv "$executionResultsPath\MSSQL\test_${name}_actualresult.csv" -Delimiter "," -NoTypeInformation
+}
+
+function Submit-TestPostgreSQL {
+	Param (
+        [Parameter(Mandatory = $true)]
+        [string] $connectionStringURL,
+        [Parameter(Mandatory = $true)]
+        [string] $name,
+		[Parameter(Mandatory = $true)]
+        [string] $query,
+		[Parameter(Mandatory = $true)]
+        [string] $executionResultsPath
+    )
+    
+    $query | psql "-A" "-F," $connectionStringURL | Out-File -FilePath "$executionResultsPath\PostgreSQL\test_${name}_actualresult.csv" -Encoding default
+}
+
+Export-ModuleMember Submit-TestMSSQL, Submit-TestPostgreSQL
