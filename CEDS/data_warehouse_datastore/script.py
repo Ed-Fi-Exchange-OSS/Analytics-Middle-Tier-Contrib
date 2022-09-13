7960012f23962f1308a511b8203c0bc19a268c0d
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-09-12 18:03:08 -0600

    Some progres.

--- CEDS/data_warehouse_datastore/script.py
+++ CEDS/data_warehouse_datastore/script.py
@@ -17,6 +17,7 @@ def get_views() -> list:
 def insert_dummy_values(config):
     conn_target = pyodbc.Connection
     try:
+        print("Inserting dummy values...", end = '')
         conn_target = pyodbc.connect(
             f'Driver={"SQL Server"};Server={config["Target"]["Server"]};Database={config["Target"]["Database"]};Trusted_Connection={config["Target"]["Trusted_Connection"]};')
         
@@ -26,6 +27,8 @@ def insert_dummy_values(config):
             conn_target.commit()
             conn_target.close()
 
+        print ("Done!")
+
     except Exception as error:
         print (error)
 
@@ -33,6 +36,7 @@ def insert_dummy_values(config):
 def transfer_data(view_info, config):
     conn_source = pyodbc.Connection
     conn_target = pyodbc.Connection
+    mapping = []
     try: 
         conn_source = pyodbc.connect(
             f'Driver={"SQL Server"};Server={config["Source"]["Server"]};Database={config["Source"]["Database"]};Trusted_Connection={config["Source"]["Trusted_Connection"]};')
@@ -40,19 +44,29 @@ def transfer_data(view_info, config):
         conn_target = pyodbc.connect(
             f'Driver={"SQL Server"};Server={config["Target"]["Server"]};Database={config["Target"]["Database"]};Trusted_Connection={config["Target"]["Trusted_Connection"]};')
 
-        cursor = conn_source.cursor()
-        print (f"Querying {view_info['source_view']}...")
-        rows = cursor.execute(f"SELECT {view_info['select']} FROM {view_info['source_view']};").fetchall()
+        cursor_source = conn_source.cursor()
+        print (f"Querying {view_info['source_view']}...", end=' ')
+        rows = cursor_source.execute(f"SELECT {view_info['select']} FROM {view_info['source_view']};").fetchall()
 
         for row in rows:
+            key_field = row[0]
+            row = row[1:]
+
             str_values = ''.join(['?,' for n in row])
             str_values = str_values[:len(str_values) - 1]
 
             if not view_info['target_fields']:
                 view_info['target_fields'] = view_info['select']
             
-            conn_target.execute(f"INSERT INTO {view_info['target_view']}({view_info['target_fields']}) VALUES ({str_values})", *row)
+            conn_target.execute(f"INSERT INTO {view_info['target_view']}({view_info['target_fields']}) VALUES ({str_values});", *row)
+            cursor_target = conn_target.cursor()
+            identity = int(cursor_target.execute(f"SELECT @@IDENTITY AS id;").fetchone()[0])
+            
+            mapping.append([identity,key_field])
             conn_target.commit()
+
+        print ('Done!')
+        return mapping
 
     except Exception as error:
         print (error)
@@ -62,7 +76,12 @@ def transfer_data(view_info, config):
         conn_target.close()
     
 if __name__ == "__main__":
+    key_mapping = {}
     config = get_configuration()
     insert_dummy_values(config)
     for view in get_views():
-        transfer_data(view, config)
+        rows = transfer_data(view, config)
+        key_mapping[view['source_view']] = rows
+
+    print (key_mapping["analytics.ceds_SchoolYearDim"])
+        
