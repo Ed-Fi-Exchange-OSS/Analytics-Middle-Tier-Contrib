3504a6aa12b40b906a23f01a7cc520c78f4f7100
jegasape <jegasape@gmail.com>
2022-09-29 14:54:06 -0400

    [BIA-1209] Addressing comments

--- Installer/Install.ps1
+++ Installer/Install.ps1
@@ -14,52 +14,61 @@ $ErrorActionPreference = "Stop"
 #--- IMPORT MODULES ---
 Import-Module -Force "$PSScriptRoot\ConfigHelper.psm1"
 $configuration = Format-ConfigurationFileToHashTable $configPath
-Import-Module -Force "$PSScriptRoot\scripts\MigratorModules\SqlDatabaseModuleWrapper.psm1"
+Import-Module -Force "$PSScriptRoot\scripts\MigratorModules\EdFi-CedsViews.psm1"
 Import-Module -Force "$PSScriptRoot\scripts\MigratorModules\EdFi-AMT.psm1" -ArgumentList $configuration
 Import-Module -Force "$PSScriptRoot\scripts\Utilities.psm1" -ArgumentList $configuration
 
-Write-Host "Installing AMT for MSSQL..." -ForegroundColor Cyan
+$databaseName = $configuration.databaseName
 
-$parameters = @{
-    databasesConfig  = @{
-        engine = "SQLServer"
-    }
-    amtDownloadPath  = $configuration.amtConfig.amtDownloadPath
-    amtInstallerPath = $configuration.amtConfig.amtInstallerPath
-    amtOptions       = $configuration.amtConfig.options
+if ($null -eq $databaseName) {
+    $databaseName = "SQLServer"
 }
 
-Install-amt @parameters
+if ($databaseName -eq "SQLServer") {
+    Write-Host "Installing AMT for MSSQL..." -ForegroundColor Cyan
 
-Write-Host "AMT has been installed for MSSQL" -ForegroundColor Cyan
-
-Write-Host "Installing AMT for PostgreSQL..." -ForegroundColor Cyan
-
-$parameters = @{
-    databasesConfig  = @{
-        engine = "PostgreSQL"
+    $parameters = @{
+        databasesConfig  = @{
+            engine = "SQLServer"
+        }
+        amtDownloadPath  = $configuration.amtConfig.amtDownloadPath
+        amtInstallerPath = $configuration.amtConfig.amtInstallerPath
+        amtOptions       = $configuration.amtConfig.options
     }
-    amtDownloadPath  = $configuration.amtConfig.amtDownloadPath
-    amtInstallerPath = $configuration.amtConfig.amtInstallerPath
-    amtOptions       = $configuration.amtConfig.options
+    
+    Install-amt @parameters
+    
+    Write-Host "AMT has been installed for MSSQL" -ForegroundColor Cyan
 }
-
-Install-amt @parameters
-
-Write-Host "AMT has been installed for PostgreSQL" -ForegroundColor Cyan
-
-Write-Host "Installing CEDS Collection for MSSQL..." -ForegroundColor Cyan
-
-$connectionString = Get-ConnectionStringMSSQL
-
-Install-CedsViews $connectionString "$PSScriptRoot\..\CEDS\MSSQL\"
-
-Write-Host "CEDS Collection has been installed for MSSQL" -ForegroundColor Cyan
-
-Write-Host "Installing CEDS Collection for PostgreSQL..." -ForegroundColor Cyan
-
-$connectionString = Get-ConnectionStringPostgreSQL
-
-Install-CedsViews $connectionString "$PSScriptRoot\..\CEDS\PostgreSQL\"
-
-Write-Host "CEDS Collection has been installed for PostgreSQL" -ForegroundColor Cyan
+else {
+    Write-Host "Installing AMT for PostgreSQL..." -ForegroundColor Cyan
+    
+    $parameters = @{
+        databasesConfig  = @{
+            engine = "PostgreSQL"
+        }
+        amtDownloadPath  = $configuration.amtConfig.amtDownloadPath
+        amtInstallerPath = $configuration.amtConfig.amtInstallerPath
+        amtOptions       = $configuration.amtConfig.options
+    }
+    
+    Install-amt @parameters
+    
+    Write-Host "AMT has been installed for PostgreSQL" -ForegroundColor Cyan
+    
+    Write-Host "Installing CEDS Collection for MSSQL..." -ForegroundColor Cyan
+    
+    $connectionString = Get-ConnectionStringMSSQL
+    
+    Install-CedsViews $connectionString "$PSScriptRoot\..\CEDS\MSSQL\"
+    
+    Write-Host "CEDS Collection has been installed for MSSQL" -ForegroundColor Cyan
+    
+    Write-Host "Installing CEDS Collection for PostgreSQL..." -ForegroundColor Cyan
+    
+    $connectionString = Get-ConnectionStringPostgreSQL
+    
+    Install-CedsViews $connectionString "$PSScriptRoot\..\CEDS\PostgreSQL\"
+    
+    Write-Host "CEDS Collection has been installed for PostgreSQL" -ForegroundColor Cyan
+}
